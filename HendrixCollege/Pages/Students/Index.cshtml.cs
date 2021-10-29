f9edafab6d1bf3d6b0e582788801aed04cffb316
juliebdick <juliebdick@gmail.com>
2021-10-28 19:13:54 -0500

    Paste sorting

--- HendrixCollege/Pages/Students/Index.cshtml.cs
+++ HendrixCollege/Pages/Students/Index.cshtml.cs
@@ -28,23 +28,69 @@ namespace HendrixCollege.Pages.Students
     //    }
     //}
 
+    //public class IndexModel : PageModel
+    //{
+    //    private readonly SchoolContext _context;
+    //    private readonly MvcOptions _mvcOptions;
+
+    //    public IndexModel(SchoolContext context, IOptions<MvcOptions> mvcOptions)
+    //    {
+    //        _context = context;
+    //        _mvcOptions = mvcOptions.Value;
+    //    }
+
+    //    public IList<Student> Student { get; set; }
+
+    //    public async Task OnGetAsync()
+    //    {
+    //        Student = await _context.Students.Take(
+    //            _mvcOptions.MaxModelBindingCollectionSize).ToListAsync();
+    //    }
+    //}
+
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
-        private readonly MvcOptions _mvcOptions;
-
-        public IndexModel(SchoolContext context, IOptions<MvcOptions> mvcOptions)
+        public IndexModel(SchoolContext context)
         {
             _context = context;
-            _mvcOptions = mvcOptions.Value;
         }
 
-        public IList<Student> Student { get; set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string AgeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.Take(
-                _mvcOptions.MaxModelBindingCollectionSize).ToListAsync();
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
