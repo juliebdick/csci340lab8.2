1e280720e0431013975b709e5779a769c346f2ae
juliebdick <juliebdick@gmail.com>
2021-10-31 21:02:50 -0500

    create an initial migration

--- HendrixCollege/Pages/About.cshtml.cs
+++ HendrixCollege/Pages/About.cshtml.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using HendrixCollege.Models.SchoolViewModels;
+using HendrixCollege.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HendrixCollege.Models;
+
 namespace HendrixCollege.Pages
 {
-    public class About
+    public class AboutModel : PageModel
     {
-        public About()
+        private readonly SchoolContext _context;
+
+        public AboutModel(SchoolContext context)
         {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Students { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Students = await data.AsNoTracking().ToListAsync();
         }
     }
 }
