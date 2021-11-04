fa32e63db62426123191830b68e483ab1cbab4a1
juliebdick <juliebdick@gmail.com>
2021-11-04 11:06:40 -0500

    Update Course Edit and Create

--- HendrixCollege/Pages/Courses/Create.cshtml.cs
+++ HendrixCollege/Pages/Courses/Create.cshtml.cs
@@ -10,7 +10,7 @@ using HendrixCollege.Models;
 
 namespace HendrixCollege.Pages.Courses
 {
-    public class CreateModel : PageModel
+    public class CreateModel : DepartmentNamePageModel
     {
         private readonly HendrixCollege.Data.SchoolContext _context;
 
@@ -21,7 +21,8 @@ namespace HendrixCollege.Pages.Courses
 
         public IActionResult OnGet()
         {
-        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            //ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            PopulateDepartmentsDropDownList(_context);
             return Page();
         }
 
@@ -31,15 +32,30 @@ namespace HendrixCollege.Pages.Courses
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            //_context.Courses.Add(Course);
+            //await _context.SaveChangesAsync();
+
+            //return RedirectToPage("./Index");
+            var emptyCourse = new Course();
+
+            if (await TryUpdateModelAsync<Course>(
+                 emptyCourse,
+                 "course",   // Prefix for form value.
+                 s => s.CourseID, s => s.DepartmentID, s => s.Title, s => s.Credits))
             {
-                return Page();
+                _context.Courses.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
-            _context.Courses.Add(Course);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            // Select DepartmentID if TryUpdateModelAsync fails.
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
         }
     }
 }
