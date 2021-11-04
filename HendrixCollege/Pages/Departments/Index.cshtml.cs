6ab6800f6b471bfc7b0d299f2e5c5b11edcfad16
juliebdick <juliebdick@gmail.com>
2021-11-04 16:58:27 -0500

    Concurrency edit pages

---
+++ HendrixCollege/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HendrixCollege.Data;
+using HendrixCollege.Models;
+
+namespace HendrixCollege.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HendrixCollege.Data.SchoolContext _context;
+
+        public IndexModel(HendrixCollege.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
---
+++ HendrixCollege/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HendrixCollege
+{
+    public static class Utility
+    {
+        public static string GetLastChars(Guid token)
+        {
+            return token.ToString().Substring(
+                                    token.ToString().Length - 3);
+        }
+    }
+}
